941da30efa389404392033414ae491daaddfaa3b
washburj10 <washburj.10@gmail.com>
2021-08-09 14:00:47 -0500

    Added NetBeans test class

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JacobWeek1TestPackage;
+
+/**
+ *
+ * @author BuRny
+ */
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("ATM Menu");
+        System.out.println("1- Display Account");
+        System.out.println("2- Deposit");
+    }
+}
